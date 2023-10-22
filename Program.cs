5896e5055b87191cdd691d39ce1bfdc68e2445a5
maks11455 <maks101330@mail.ru>
2023-10-22 21:18:37 +0000

    Коммит для сдачи аттестации

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿//Первая задачка
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите значение N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        PrintNaturalNumbersFromNTo1(N);
+
+        Console.ReadLine(); // Чтобы консольное окно не закрылось сразу
+    }
+
+    static void PrintNaturalNumbersFromNTo1(int N)
+    {
+        if (N >= 1)
+        {
+            Console.Write(N);
+
+            if (N > 1)
+            {
+                Console.Write(", ");
+            }
+
+            PrintNaturalNumbersFromNTo1(N - 1);
+        }
+    }
+}
